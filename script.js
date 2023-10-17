4b45dc03360b76e426e8b20f41dd5ce9faed5dac
imrankhanfornever <145145595+imrankhanfornever@users.noreply.github.com>
2023-10-17 14:56:13 +0530

    if-js-task-completed

---
+++ script.js
@@ -0,0 +1,34 @@
+var marks = 30;
+
+if (marks < 0) && (mraks > 100); {
+    console.log ("Invalid Marks")
+};
+
+
+else if (marks => 0) && (mraks <= 39); {
+    console.log ("Grade F")
+};
+
+else if (marks => 40) && (mraks <= 59) {
+    console.log ("Grade D")
+} 
+
+else if (marks => 60) && (mraks <= 79) {
+    console.log ("Grade C")
+}
+
+else if (marks => 80) && (mraks <= 94) {
+    console.log ("Grade B")
+}
+
+else if (marks => 95) && (mraks <= 99) {
+    console.log ("Grade A")
+}
+
+else if (marks =100) {
+    console.log ("Grade S")
+}
+
+else { Put marks
+    
+}
